2b188ac8e925b936a30d36cea79c714f925e4892
kakikubo teruo <kakikubo@gmail.com>
2020-08-26 22:54:34 +0900

    わざと失敗。そしてpendingへ

--- spec/experiments/string_spec.rb
+++ spec/experiments/string_spec.rb
@@ -2,9 +2,15 @@ require "spec_helper"
 
 describe String do
   describe "#<<" do
-    example "文字の追加" do
+    it "appends a character" do
       s = "ABC"
       s << "D"
+      expect(s.size).to eq(4)
+    end
+    it "appends a nil" do
+      pending("調査中")
+      s = "ABC"
+      s << nil
       expect(s.size).to eq(4)
     end
   end
