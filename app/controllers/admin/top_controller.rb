40b503351e1bafda25fb553b478735dd366d1236
kakikubo teruo <kakikubo@gmail.com>
2020-09-02 23:15:23 +0900

    staff_membersとadministratorsを作成してテスト追加。ログイン画面だけ出す

--- app/controllers/admin/top_controller.rb
+++ app/controllers/admin/top_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class Admin::TopController < ApplicationController
+class Admin::TopController < Admin::Base
   def index
     # render action: "index"
   end
--- app/controllers/staff/top_controller.rb
+++ app/controllers/staff/top_controller.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
-class Staff::TopController < ApplicationController
+class Staff::TopController < Staff::Base
   def index
     # raise IpAddressRejected
     # raise Forbidden
-    raise ActiveRecord::RecordNotFound
+    # raise ActiveRecord::RecordNotFound
     render action: "index"
   end
 end
