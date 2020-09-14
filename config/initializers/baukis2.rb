a5dc9030c552065d02ea0dde2f39a4563edb2f89
kakikubo teruo <kakikubo@gmail.com>
2020-09-14 23:51:59 +0900

    routingの整理を行い、baukis2.lvh.meでのみアクセスできるように変更

---
+++ config/initializers/baukis2.rb
@@ -0,0 +1,7 @@
+Rails.application.configure do
+  config.baukis2 = {
+      staff: { host: "baukis2.lvh.me", path: "" },
+      admin: { host: "baukis2.lvh.me", path: "admin"},
+      customer: { host: "lvh.me", path: "mypage"}
+  }
+end
