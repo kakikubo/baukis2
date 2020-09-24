81502aa20d558b3e841196d8e758fe317f00695c
kakikubo teruo <kakikubo@gmail.com>
2020-09-24 23:50:28 +0900

    ログイン処理をbeforeで定義して書く

--- spec/requests/admin/staff_members_management_spec.rb
+++ spec/requests/admin/staff_members_management_spec.rb
@@ -1,39 +1,49 @@
 require 'rails_helper'
 
-describe "管理者による職員管理" do
+describe '管理者による職員管理' do
   let(:administrator) { create(:administrator) }
 
-  describe "新規登録" do
+  before do
+    post admin_session_url,
+         params: {
+           admin_login_form: {
+             email: administrator.email,
+             password: 'pw'
+           }
+         }
+  end
+
+  describe '新規登録' do
     let(:params_hash) { attributes_for(:staff_member) }
 
-    example "職員一覧ページにリダイレクト" do
-      post admin_staff_members_url, params: {staff_member: params_hash}
+    example '職員一覧ページにリダイレクト' do
+      post admin_staff_members_url, params: { staff_member: params_hash }
       expect(response).to redirect_to(admin_staff_members_url)
     end
 
-    example "例外 ActionController::ParameterMissing が発生" do
+    example '例外 ActionController::ParameterMissing が発生' do
       expect { post admin_staff_members_url }.to raise_error(ActionController::ParameterMissing)
     end
   end
 
-  describe "更新" do
+  describe '更新' do
     let(:staff_member) { create(:staff_member) }
     let(:params_hash) { attributes_for(:staff_member) }
 
-    example "suspendedフラグをセットする" do
+    example 'suspendedフラグをセットする' do
       params_hash.merge!(suspended: true)
-      patch admin_staff_member_url(staff_member), params: {staff_member: params_hash}
+      patch admin_staff_member_url(staff_member), params: { staff_member: params_hash }
       staff_member.reload
       expect(staff_member).to be_suspended
     end
 
-    example "hashed_passwordの値は書き換え不可" do
+    example 'hashed_passwordの値は書き換え不可' do
       params_hash.delete(:password)
-      params_hash.merge!(hashed_password: "x")
-      expect {
+      params_hash.merge!(hashed_password: 'x')
+      expect do
         patch admin_staff_member_url(staff_member),
-              params: {staff_member: params_hash}
-      }.not_to change { staff_member.hashed_password.to_s }
+              params: { staff_member: params_hash }
+      end.not_to change { staff_member.hashed_password.to_s }
     end
   end
 end
