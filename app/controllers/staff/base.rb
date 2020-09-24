8bd9d7b903ff81afba34f16ef3e26e7c03bfc423
kakikubo teruo <kakikubo@gmail.com>
2020-09-24 23:46:47 +0900

    アカウントが無効、もしくはセッション有効期限(60分)を超えると強制ログアウトする機能を実装

--- app/controllers/staff/base.rb
+++ app/controllers/staff/base.rb
@@ -1,5 +1,5 @@
 class Staff::Base < ApplicationController
-  before_action :authorize
+  before_action :authorize, :check_account, :check_timeout
 
   private def current_staff_member
     @current_staff_member ||= StaffMember.find_by(id: session[:staff_member_id]) if session[:staff_member_id]
@@ -11,6 +11,28 @@ class Staff::Base < ApplicationController
     unless current_staff_member
       flash.alert = '職員としてログインしてください'
       redirect_to :staff_login
+    end
+  end
+
+  private def check_account
+    if current_staff_member && !current_staff_member.active?
+      session.delete(:staff_member_id)
+      flash.alert = 'アカウントが無効になりました'
+      redirect_to :staff_root
+    end
+  end
+
+  TIMEOUT = 60.minutes
+
+  private def check_timeout
+    if current_staff_member
+      if session[:last_access_time] >= TIMEOUT.ago
+        session[:last_access_time] = Time.current
+      else
+        session.delete(:staff_member_id)
+        flash.alert = 'セッションがタイムアウトしました'
+        redirect_to :staff_login
+      end
     end
   end
 end
