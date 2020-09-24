459b61dcb78bcc1f55769591865dee0fdf503bfc
kakikubo teruo <kakikubo@gmail.com>
2020-09-24 23:33:03 +0900

    認証機能をbefore_actionでまとめるやつ

--- app/controllers/admin/base.rb
+++ app/controllers/admin/base.rb
@@ -1,7 +1,17 @@
 class Admin::Base < ApplicationController
+  before_action :authorize
+
   private def current_administrator
     @current_administrator ||= Administrator.find_by(id: session[:administrator_id]) if session[:administrator_id]
   end
 
   helper_method :current_administrator
+
+  private def authorize
+    unless current_administrator
+      flash.notice = "管理者としてログインしてください"
+      redirect_to :admin_login
+    end
+  end
+
 end
--- app/controllers/staff/base.rb
+++ app/controllers/staff/base.rb
@@ -1,7 +1,16 @@
 class Staff::Base < ApplicationController
+  before_action :authorize
+
   private def current_staff_member
     @current_staff_member ||= StaffMember.find_by(id: session[:staff_member_id]) if session[:staff_member_id]
   end
 
   helper_method :current_staff_member
+
+  private def authorize
+    unless current_staff_member
+      flash.alert = '職員としてログインしてください'
+      redirect_to :staff_login
+    end
+  end
 end
