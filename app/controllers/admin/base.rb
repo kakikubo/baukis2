c1445436f8b8a5b3a25a5893bb77d4f1b7ad8da9
kakikubo teruo <kakikubo@gmail.com>
2020-09-26 00:10:33 +0900

    Chapter12終わり。

--- app/controllers/admin/base.rb
+++ app/controllers/admin/base.rb
@@ -1,5 +1,5 @@
 class Admin::Base < ApplicationController
-  before_action :authorize
+  before_action :authorize, :check_account, :check_timeout
 
   private def current_administrator
     @current_administrator ||= Administrator.find_by(id: session[:administrator_id]) if session[:administrator_id]
@@ -9,9 +9,30 @@ class Admin::Base < ApplicationController
 
   private def authorize
     unless current_administrator
-      flash.notice = "管理者としてログインしてください"
+      flash.notice = '管理者としてログインしてください'
       redirect_to :admin_login
     end
   end
 
+  private def check_account
+    if current_administrator && current_administrator.suspended?
+      session.delete(:administrator_id)
+      flash.alert = 'アカウントが無効になりました'
+      redirect_to :admin_root
+    end
+  end
+
+  TIMEOUT = 60.minutes
+
+  private def check_timeout
+    if current_administrator
+      if session[:admin_last_access_time] >= TIMEOUT.ago
+        session[:admin_last_access_time] = Time.current
+      else
+        session.delete(:administrator_id)
+        flash.alert = 'セッションがタイムアウトしました'
+        redirect_to :admin_login
+      end
+    end
+  end
 end
