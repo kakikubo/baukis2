386274104d191540eea22d676384d113a5be00fa
kakikubo teruo <kakikubo@gmail.com>
2020-11-13 07:08:55 +0900

    パスワード変更機能のモックを作成

--- app/forms/staff/change_password_form.rb
+++ app/forms/staff/change_password_form.rb
@@ -4,7 +4,7 @@ class Staff::ChangePasswordForm
   attr_accessor :object, :current_password, :new_password, :new_password_confirmation
 
   def save
-    object.password = new.password
+    object.password = new_password
     object.save!
   end
 end
