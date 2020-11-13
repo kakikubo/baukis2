5a8cfd231ec3961cb76351c3b35638afd32387ac
kakikubo teruo <kakikubo@gmail.com>
2020-11-14 07:58:49 +0900

    delegateメソッドを通じてコードの簡略化を行う。まだそんなに理解できていない

--- app/presenters/staff_member_presenter.rb
+++ app/presenters/staff_member_presenter.rb
@@ -1,8 +1,10 @@
 class StaffMemberPresenter < ModelPresenter
+  delegate :suspended?, to: :object
+
   # 職員の停止フラグの On/Off を表現する記号を返す
   # On: BALLOUT_BOX_WITH_CHECK (U+2611)
   # Off: BALLOUT_BOX (U+2610)
   def suspended_mark
-    object.suspended? ? view_context.raw('&#x2611;') : view_context.raw('&#x2610;')
+    suspended? ? raw('&#x2611;') : raw('&#x2610;')
   end
 end
