104bb74db6db566bbd5fbd0e622feadfef160b6d
kakikubo teruo <kakikubo@gmail.com>
2020-11-15 07:55:16 +0900

    FormPresenterによるリファクタ2

--- app/presenters/form_presenter.rb
+++ app/presenters/form_presenter.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 class FormPresenter
   include HtmlBuilder
 
@@ -10,5 +11,36 @@ class FormPresenter
   def initialize(form_builder, view_context)
     @form_builder = form_builder
     @view_context = view_context
+  end
+
+  def notes
+    markup(:div, class: 'notes') do |m|
+      m.span '*', class: 'mark'
+      m.text '印の付いた項目は入力必須です。'
+    end
+  end
+
+  def text_field_block(name, label_text, options = {})
+    markup(:div, class: 'input-block') do |m|
+      m << label(name, label_text,
+                 class: options[:required] ? 'required' : nil)
+      m << text_field(name, options)
+    end
+  end
+
+  def password_field_block(name, label_text, options = {})
+    markup(:div, class: 'input-block') do |m|
+      m << label(name, label_text,
+                 class: options[:required] ? 'required' : nil)
+      m << password_field(name, options)
+    end
+  end
+
+  def date_field_block(name, label_text, options = {})
+    markup(:div, class: 'input-block') do |m|
+      m << label(name, label_text,
+                 class: options[:required] ? 'required' : nil)
+      m << date_field(name, options)
+    end
   end
 end
