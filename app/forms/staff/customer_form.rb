bd7505aa78464665888b5d2e020db3aa45533646
kakikubo teruo <kakikubo@gmail.com>
2020-11-19 06:16:57 +0900

    基本情報と住所情報、勤務先情報をバリデーション

--- app/forms/staff/customer_form.rb
+++ app/forms/staff/customer_form.rb
@@ -19,11 +19,17 @@ class Staff::CustomerForm
     customer.work_address.assign_attributes(work_address_params)
   end
 
+  def valid?
+    customer.valid? && customer.home_address.valid? && customer.work_address.valid?
+  end
+
   def save
-    ActiveRecord::Base.transaction do
-      customer.save!
-      customer.home_address.save!
-      customer.work_address.save!
+    if valid?
+      ActiveRecord::Base.transaction do
+        customer.save!
+        customer.home_address.save!
+        customer.work_address.save!
+      end
     end
   end
 
