7b7c34a7ce5425aab7d48c21713b0ea774b663b3
kakikubo teruo <kakikubo@gmail.com>
2020-11-20 07:38:58 +0900

    「自宅住所を入力する」ボタンが有効になっている時のみ値を保存する(勤務先も同様)

--- app/forms/staff/customer_form.rb
+++ app/forms/staff/customer_form.rb
@@ -16,9 +16,12 @@ class Staff::CustomerForm
 
   def assign_attributes(params = {})
     @params = params
+    self.inputs_home_address = params[:inputs_home_address] == "1"
+    self.inputs_work_address = params[:inputs_work_address] == "1"
+
     customer.assign_attributes(customer_params)
-    customer.home_address.assign_attributes(home_address_params)
-    customer.work_address.assign_attributes(work_address_params)
+    customer.home_address.assign_attributes(home_address_params) if inputs_home_address
+    customer.work_address.assign_attributes(work_address_params) if inputs_work_address
   end
 
   private def customer_params
