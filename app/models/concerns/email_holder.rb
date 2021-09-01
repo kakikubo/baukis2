f91553dd7e18164fdafe783262cad6f6e30e27c7
kakikubo <t.kakikubo@fitsplus.co.jp>
2021-09-01 23:01:41 +0900

    rubocop -a

--- app/models/concerns/email_holder.rb
+++ app/models/concerns/email_holder.rb
@@ -10,7 +10,7 @@ module EmailHolder
       self.email = normalize_as_email(email)
     end
 
-    validates :email, presence: true, "valid_email_2/email": true,
+    validates :email, presence: true, 'valid_email_2/email': true,
                       uniqueness: { case_sensitive: false }
   end
 end
