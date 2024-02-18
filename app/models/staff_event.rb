67967f17b6d4420343e7d0648d154b80d8983d59
kakikubo <kakikubo@gmail.com>
2024-02-19 06:54:41 +0900

    Rails/InverseOfの対処

--- app/models/staff_event.rb
+++ app/models/staff_event.rb
@@ -3,7 +3,7 @@
 class StaffEvent < ApplicationRecord
   self.inheritance_column = nil
 
-  belongs_to :member, class_name: 'StaffMember', foreign_key: 'staff_member_id'
+  belongs_to :member, class_name: 'StaffMember', foreign_key: 'staff_member_id', inverse_of: :events
   alias_attribute :occurred_at, :created_at
 
   DESCRIPTION = {
