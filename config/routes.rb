1e4d5d6811ed957238c4c98f37563073e13fd327
kakikubo <kakikubo@gmail.com>
2024-03-06 06:01:29 +0900

    adminも別ファイル管理へ。staffもconstraintsから別ファイルにうつした

--- config/routes.rb
+++ config/routes.rb
@@ -3,21 +3,8 @@
 Rails.application.routes.draw do
   config = Rails.application.config.baukis2
 
-  constraints host: config[:staff][:host] do
-    draw(:staff)
-  end
-
-  constraints host: config[:admin][:host] do
-    namespace :admin, path: config[:admin][:path] do
-      root 'top#index'
-      get 'login' => 'sessions#new', as: :login
-      resource :session, only: %i[create destroy]
-      resources :staff_members do
-        resources :staff_events, only: [:index]
-      end
-      resources :staff_events, only: [:index]
-    end
-  end
+  draw(:staff)
+  draw(:admin)
 
   constraints host: config[:customer][:host] do
     namespace :customer, path: config[:customer][:path] do
